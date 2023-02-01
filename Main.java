b29b5de39ae0df0b2eb6d728566fb7e774a8cf0e
Pankaj Kumar <Pankaj Kumar>
2023-02-01 19:16:44 +0530

    Implementation of method is done

--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
 class Main {
 
+    public static boolean isPalidrome(String str) {
+        int n = str.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String input = "Hello mam how are you";
+        String[] arr = input.split(" ");
+        for (int j = 0; j < arr.length; j++) {
+            boolean flag = isPalidrome(arr[j]);
+            if (flag) {
+                System.out.println(arr[j]);
+            }
+
+        }
     }
 }
